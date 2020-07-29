a6109e5b1180658f67d3bb48b5f7d954963d9b4b
maqing <mr.maqing@gmail.com>
2020-07-29 10:59:06 +0800

    [maqing] finished exeercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -1,4 +1,4 @@
-export default function arrayToList(array) {
+export default function arrayToList (array) {
   // This function creates a linked list from an array.
   //
   // Your target:
@@ -6,5 +6,17 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array == null) throw new Error('Creating list from undefined array');
+  if (array.length === 0) throw new Error('Creating list from empty array');
+
+  const header = { value: array[0], next: null };
+  let curNode = header;
+
+  for (let index = 1; index < array.length; index += 1) {
+    const nextNode = { value: array[index], next: null };
+    curNode.next = nextNode;
+    curNode = nextNode;
+  }
+
+  return header;
 }
